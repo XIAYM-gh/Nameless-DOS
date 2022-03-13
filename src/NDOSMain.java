ebd683d7a7c47967449973770872ab94495ea32b
XIAYM-gh <xiaym123@qq.com>
2022-03-13 15:19:20 +0800

    增加win下可能无法输出颜色的消息

--- src/NDOSMain.java
+++ src/NDOSMain.java
@@ -11,11 +11,17 @@ public class NDOSMain {
     xconfig x = new xconfig("config.properties");
 
     if(!x.has("windows-force-jansi-enabled")) x.put("windows-force-jansi-enabled", "false");
+    if(!x.has("win-notification-ignored")) x.put("win-notification-ignored", "false");
     x.save();
 
     String os = System.getProperty("os.name").toLowerCase();
 
     if(x.get("windows-force-jansi-enabled", "false").equals("true") || !os.contains("windows")) NDOSAPI.JANSI_ENABLED = true;
+    if(!NDOSAPI.JANSI_ENABLED && x.get("win-notification-ignored","false").equals("false")) {
+      Logger.warn("检测到您正处于Windows环境，我们已经默认禁用颜色输出，请确认您的终端可以显示ANSI颜色后在同目录的config.properties里打开 windows-force-jansi-enabled 项，此消息将不会再显示.");
+      x.put("win-notification-ignored", "true");
+      x.save();
+    }
 
     AnsiConsole.systemInstall();
 
