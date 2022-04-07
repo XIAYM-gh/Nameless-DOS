db3bc71090f2ccee76f951c10d857bc78648bfd8
XIAYM-gh <xiaym123@qq.com>
2022-04-07 18:59:47 +0800

    ConfigUtil: 增加配置类，UpdateUtil: 增加镜像源选项 (Fix)

---
+++ src/utils/ConfigUtil.java
@@ -0,0 +1,33 @@
+package cn.xiaym.utils;
+
+import java.io.*;
+import java.util.*;
+
+public class ConfigUtil {
+  public static void init() {
+    File f = new File("config");
+
+    if(f.exists() && !f.isDirectory()) f.delete();
+    if(!f.exists()) f.mkdirs();
+  }
+
+  public static boolean hasSection(String name) {
+    return new File("config/" + name + ".properties").exists();
+  }
+
+  public static xconfig getSection(String name) {
+    return new xconfig("config/" + name + ".properties");
+  }
+
+  public static String get(String key) {
+    return new xconfig("config.properties").get(key, null);
+  }
+
+  public static String get(String key, String def) {
+    return new xconfig("config.properties").get(key, def);
+  }
+
+  public static Set<String> keySet() {
+    return new xconfig("config.properties").keySet();
+  }
+}
